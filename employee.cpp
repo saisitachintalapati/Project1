d38652aa92259304f26c4772326dd557bdae07e9
saisitachintalapati <saisita9999@gmail.com>
2020-05-13 21:11:20 +0530

    calculate_salary function added
    Added function to calculate salary based on designation

--- employee.cpp
+++ employee.cpp
@@ -2,6 +2,15 @@
 #include<string>
 
 using namespace std;
+enum designation 
+{
+    Band_L3=1,
+    Band_L2,
+    Band_L1,
+    Band_E3,
+    Band_E2,
+    Band_E1
+};
 class employee
 {
  private:
@@ -9,6 +18,8 @@ class employee
       int id_no;
       int month_working_days;
       int sick_leave;
+      int des;
+      double salary;
  public:
  employee()
  {
@@ -16,14 +27,18 @@ class employee
      id_no=0;
      month_working_days=22;
      sick_leave=0;
+     des=0;
+     
  }
- employee(string n,int id,int wd)
+ employee(string n,int id,int wd,int d)
  {
      e_name=n;
      id_no=id;
      month_working_days=wd;
      sick_leave=0;
+     des=d;
  }
+ 
  void set_working_days()
  {
      month_working_days=month_working_days-sick_leave;
@@ -41,6 +56,10 @@ class employee
  {
      return e_name;
  }
+ int get_designation()
+ {
+     return des;
+ }
  int get_id()
  {
      return id_no;
@@ -51,7 +70,34 @@ class employee
    if(leave)
        sick_leave++; 
  }
-
+ double get_salary()
+ {
+     return salary;
+ }
+     
+ 
+ void calculate_salary()
+ {
+     int pf=2000;
+     switch(get_designation())
+     {
+    case 1: salary=(double)(3560000/12)-(get_sick_leave()*10000)-pf;
+    break;
+    case 2: salary=(double)(2960000/12)-(get_sick_leave()*8000)-pf;
+    break;
+    case 3: salary=(double)(2160000/12)-(get_sick_leave()*6000)-pf;
+    break;
+     case 4:salary=(double)(1770000/12)-(get_sick_leave()*5000)-pf;
+     break;
+     case 5: salary=(double)(1050000/12)-(get_sick_leave()*3000)-pf;
+     break;
+     case 6: salary=(double)(696000/12)-(get_sick_leave()*2000)-pf;
+     break;
+     default:printf("Enter proper designation");
+     break;
+     }
+     
+ }
 
 };
 
@@ -60,14 +106,16 @@ int main()
 {
     int i;
     employee e[2]=
-    { employee("Rose",1000,22),
-      employee("Chris",403,22)
+    { employee("Rose",1000,22,1),
+      employee("Chris",403,22,4)
     };
     e[0].set_leave(true);
     e[1].set_leave(true);
     e[1].set_leave(true);
     e[1].set_working_days();
     e[0].set_working_days();
+    e[0].calculate_salary();
+    e[1].calculate_salary();
     for(i=0;i<2;i++)
     {
     printf("Employee %d details:\n",i);
@@ -75,5 +123,8 @@ int main()
     cout<<e[i].get_id()<<endl;
     cout<<e[i].get_working_days()<<endl;
     cout<<e[i].get_sick_leave()<<endl;
+    cout<<e[i].get_salary()<<endl;
+
+
     }
 }
